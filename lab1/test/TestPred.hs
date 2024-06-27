42f9f0bfde102ad1916575f42b560cab10e056c0
Ignacio Gomez Barrios <117108736+lettu3@users.noreply.github.com>
2024-06-27 01:58:00 -0300

    Delete lab1 directory

--- lab1/test/TestPred.hs
+++
@@ -1,9 +0,0 @@
-module Main (main) where
-import System.Exit (exitFailure)
-import Control.Monad (when)
-import Pred (falla)
-
-main :: IO ()
-main = do
-    putStrLn "Test suite not yet implemented."
-    when falla exitFailure
